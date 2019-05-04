fc38114b46ee17b8288bdfac1c2f7cfa3e278231
William <william.v.wang@gmail.com>
2019-05-04 17:07:22 -0500

    Fix server side room disposal logic

--- server.py
+++ server.py
@@ -1,59 +1,58 @@
-from flask import Flask, render_template, redirect, abort, request
-from flask_socketio import SocketIO, send, emit, join_room, leave_room
+from flask import Flask, request
+from flask_socketio import SocketIO, send, emit, join_room, leave_room, close_room
 
-from whisper.routing import routes
+from whisper.views import routes
 from whisper.common import common
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'it\'s a secret ~'
 app.register_blueprint(routes)
 
+# TODO: move SocketIO into subpackage
 socketio = SocketIO(app)
 
-######################
-##  Event Handlers  ##
-######################
-
-@socketio.on('connect')
-def handle_connect():
-	emit('response', {'data': 'user connected'})
-
-@socketio.on('disconnect')
-def handle_disconnect():
-	emit('response', {'data': 'user connected'})
+###############################
+##  SocketIO Event Handlers  ##
+###############################
 
 @socketio.on('join')
 def handle_join(data):
-	# Extract username, room, and session id
 	username = data['username']
 	room = data['room']
 	if room not in common.rooms:
 		return
-	sid = request.sid
-	# Map username to session id in room space
-	common.rooms[room]['users'][sid] = username
+	session_id = request.sid
+	# Map username to user session id in room space
+	common.rooms[room]['users'][session_id] = username
 	join_room(room)
-	send(username + ' has entered the chatroom.', room=room)
+	print(username + ' joined ' + room)
+	send(username + ' has joined the chatroom.', room=room)
 
 @socketio.on('leave')
 def handle_leave(data):
-	# Extract username, room, and session id
 	username = data['username']
 	room = data['room']
-	sid = request.sid
+	session_id = request.sid
 	leave_room(room)
+	print(username + ' left ' + room)
 	send(username + ' has left the chatroom.', room=room)
-	del common.rooms[room]['users'][sid]
+	del common.rooms[room]['users'][session_id]
 	# Delete room if all users have left
-	if len(common.rooms[room]['users'] == 0):
+	if len(common.rooms[room]['users']) == 0:
+		print('disposing room ' + room)
+		close_room(room)
 		del common.rooms[room]
+		
 
 @socketio.on('message')
 def handle_msg(data):
 	room = data['room']
-	sid = request.sid
+	session_id = request.sid
 	# Echo received messages back to room
-	emit('broadcast', {'sender': common.rooms[room]['users'][sid], 'msg': data['msg']}, room=room)
+	emit('broadcast', {
+		'sender': common.rooms[room]['users'][session_id],
+		'msg': data['msg']
+	}, room=room)
 
 #############
 ##  Entry  ##
